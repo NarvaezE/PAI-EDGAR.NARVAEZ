c7bdaecff0cef2261c7dd5a6580f7cb3a7fca633
NarvaezE <enarvaez_19@alu.uabcs.mx>
2022-09-15 19:47:39 -0500

    trabajando con less

---
+++ app/public/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/main.css">
+</head>
+<body>
+    <div class="container">
+        <fieldset>
+            <form>
+                <legend>Acceso al panel</legend>
+
+                <label>Correo</label>
+
+                <input type="email" name="">
+
+                <label>Contraseña</label>
+
+                <input type="password" name="">
+
+                <button type="submit" class="">
+                    A C C E D E R
+                </button>
+            </form>
+        </fieldset>
+    </div>
+</body>
+</html>
