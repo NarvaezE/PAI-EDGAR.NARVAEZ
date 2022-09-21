fed66f460050c7de056b83ad3665603bcd14f604
Edgar Narvaez <88750371+NarvaezE@users.noreply.github.com>
2022-09-21 16:53:05 -0600

    login b5

--- app/public/index.php
+++ app/public/index.php
@@ -5,27 +5,48 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="css/main.css">
-</head>
+   
+    <link rel="stylesheet" href="https://www.markuptag.com/bootstrap/5/css/bootstrap.min.css">
+    <style>
+        .registro{
+            min-width: 600px;
+        }
+    </style>
+</head> 
 <body>
-    <div class="container">
-        <fieldset>
-            <form>
-                <legend>Acceso al panel</legend>
+    <div class="container recolor">
+        <section class="border ">
+            <div class="row registro justify-content-md-center align-items-center pd-12">
+                <div class="col-md-6 col-sm-12">
+                    <fieldset >
+                        <form>
+                            <h1 class="text-center">Acceso al panel</h1>
+                            <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                            </p>
+                            <label>Correo Electronico</label>
 
-                <label>Correo</label>
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="email" class="form-control" placeholder="Email" aria-label="Email" aria-describedby="basic-addon1">
+                            </div>
+                            <label>Contraseña</label>
 
-                <input type="email" name="">
+                            <div class="input-group mb-3">
+                                <span class="input-group-text" id="basic-addon1">@</span>
+                                <input type="password" class="form-control" placeholder="**********" aria-label="Password" aria-describedby="basic-addon1">
+                            </div>
 
-                <label>Contraseña</label>
-
-                <input type="password" name="">
-
-                <button type="submit" class="">
-                    A C C E D E R
-                </button>
-            </form>
-        </fieldset>
+                            <button type="submit" class="btn btn-dark col-12">
+                                A C C E D E R
+                            </button>
+                        </form>
+                    
+                    </fieldset>  
+                </div>  
+            </div>
+        </section>
     </div>
+    <script src="https://www.markuptag.com/bootstrap/5/js/bootstrap.bundle.min.js"></script>
 </body>
 </html>
